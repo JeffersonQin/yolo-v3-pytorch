5a28b951b9eb22a8ce1a67ee72c1bcad4127c28f
gyro永不抽风 <1247006353@qq.com>
2022-06-05 13:55:21 +0800

    feat: add learning rate scheduler structure

--- utils/utils.py
+++ utils/utils.py
@@ -29,12 +29,42 @@ def update_lr(opt: torch.optim.Optimizer, lr: float):
 		param_group['lr'] = lr
 
 
-def linear_warmup_cosine_lr_scheduler(linear_max, warmup_epoch, T_half):
-	def lr(epoch):
-		if epoch < warmup_epoch: return linear_max / warmup_epoch * (epoch + 1)
-		epoch = (epoch - warmup_epoch) % T_half
-		return linear_max * 0.5 * (1 + cos(epoch / T_half * pi))
-	return lr
+class LearningRateScheduler:
+	def __init__(self, lr_scheduler, max_epoch):
+		self.lr_scheduler = lr_scheduler
+		self.max_epoch = max_epoch
+
+
+	def __call__(self, epoch):
+		return self.lr_scheduler(epoch)
+
+
+class LinearWarmupScheduler(LearningRateScheduler):
+	def __init__(self, linear_max, max_epoch):
+		def lr(epoch):
+			return linear_max / max_epoch * (epoch + 1)
+		super().__init__(lr, max_epoch)
+
+
+class CosineAnnealingScheduler(LearningRateScheduler):
+	def __init__(self, cosine_max, T_half):
+		def lr(epoch):
+			epoch = epoch % T_half
+			return cosine_max * 0.5 * (1 + cos(epoch / T_half * pi))
+		super().__init__(lr, T_half)
+
+
+class LearningRateSchedulerComposer:
+	def __init__(self, lr_schedulers: list[LearningRateScheduler]):
+		self.lr_schedulers = lr_schedulers
+
+
+	def __call__(self, epoch):
+		for lr_scheduler in self.lr_schedulers:
+			if epoch < lr_scheduler.max_epoch:
+				return lr_scheduler(epoch)
+			else: epoch -= lr_scheduler.max_epoch
+		return 0.0
 
 
 # from d2l
